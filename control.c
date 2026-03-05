21d4a7e956d10341bc118f0d924f7ba6b6a93399
Luca Gurband <147512034+LucaGurr@users.noreply.github.com>
2026-03-05 14:57:02 +0000

    menu extension pt.1

--- control.c
+++ control.c
@@ -1,7 +1,16 @@
 #include "stdio.h"
+int choice;
 
 void clearScreen(){
     printf("\x1b[3J\x1b[H\x1b[2J");
+}
+
+void defaultSecLenMenu(){
+    clearScreen();
+}
+
+void customSecLenMenu() {
+    clearScreen();
 }
 
 
@@ -10,6 +19,14 @@ void inverseKinematicsMenu() {
     clearScreen();
 
     printf("INVERSE KINEMATICS MENU:\n    1) Use defauls section Lengths\n    2) Use custom section Lengths\n");
+    if (scanf("%d", &choice) == 1 && choice == 1) {
+        
+    } 
+    else if (choice == 2) {
+    }
+    else {
+        printf("navn\n");
+    }
 
 }
 
@@ -19,19 +36,28 @@ void rawDataMenu(){
 
     printf("RAW DATA MENU:\n    1) Use All Motors\n    2) Use custom Motors\n");
 
+    if (scanf("%d", &choice) == 1 && choice == 1) {
+        
+    } 
+    else if (choice == 2) {
+
+    }
+    else {
+        printf("navn\n");
+    }
+
 }
 
 
 void menuStructure(){
-    int choice;
 
     clearScreen();
 
     printf("MAIN MENU\n    1) choose inverse Kinematics mode\n    2) choose raw movement data mode\n");
-    if (scanf("%d", &choice) == 1) {
+    if (scanf("%d", &choice) == 1 && choice == 1) {
         inverseKinematicsMenu();
     } 
-    else if (scanf("%d", &choice) == 2) {
+    else if (choice == 2) {
         rawDataMenu();
     }
     else {
