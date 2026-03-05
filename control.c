dc9bec81403997ddea06a0c94fef809eed406bd9
Luca Gurband <147512034+LucaGurr@users.noreply.github.com>
2026-03-05 18:00:06 +0000

    fix: correct section length input handling in customSecLenMenu

--- control.c
+++ control.c
@@ -29,10 +29,21 @@ void customSecLenMenu() {
     clearScreen();
 
     printf("%sCUSTOM SECTION LENGTH MENU%s\n    Please enter the upper section length below (mm):\n    ", BLUE, RESET);
-    scanf("%d", &tempLengths[0]);
+    scanf("%d", &tempLengths[1]);
 
     clearScreen();
-
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    You have entered the following section lengths:\n    %sLower Section Length:%s %d mm\n    %sUpper Section Length:%s %d mm\n    %s1)%s Confirm and continue\n    %s2)%s Re-enter section lengths\n    ", BLUE, RESET, RED, RESET, tempLengths[0], RED, RESET, tempLengths[1], RED, RESET);
+    if (scanf("%d", &choice) == 1 && choice == 1) {
+        lengths[0] = tempLengths[0];
+        lengths[1] = tempLengths[1];
+    }
+    else if (choice == 2) {
+        customSecLenMenu();
+    }
+    else {
+        clearScreen();
+        printf("navn\n");
+    }
 }
 
 
