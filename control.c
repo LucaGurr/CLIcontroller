a98c321700336a4689caa9c9232f0e313a24d8bc
Luca Gurband <147512034+LucaGurr@users.noreply.github.com>
2026-03-05 18:26:16 +0000

    custom section length menu with target and initial coordinate input

--- control.c
+++ control.c
@@ -9,9 +9,47 @@
 int choice;
 
 int lengths[2] = {500, 525};
+int initcoords[3] = {0};
+int targetcoords[3] = {0};
+
 
 void clearScreen(){
     printf("\x1b[3J\x1b[H\x1b[2J");
+}
+
+
+void getTargetCoords(){
+    clearScreen();
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    Please enter the target coordinates of the end effector in the format (x, y, z) where x,z are the coordinates in millimeters and y is the rotation angle of base in degrees:\n    ", BLUE, RESET);
+    scanf("%d, %d, %d", &targetcoords[0], &targetcoords[1], &targetcoords[2]);
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    You have entered the following target coordinates:\n    %sX Coordinate:%s %d mm\n    %sBase Rotation Angle:%s %d degrees\n    %sZ Coordinate:%s %d mm\n    %s1)%s Confirm and continue\n    %s2)%s Re-enter target coordinates\n    ", BLUE, RESET, RED, RESET, targetcoords[0], RED, RESET, targetcoords[1], RED, RESET, targetcoords[2], RED, RESET, RED, RESET);
+    if (scanf("%d", &choice) == 1 && choice == 1) {
+        // Continue with the selected target coordinates
+    }
+    else if (choice == 2) {
+        getTargetCoords();
+    }
+    else {
+        clearScreen();
+        printf("Invalid choice\n");
+    }
+}
+
+void getCoords(){
+    clearScreen();
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    Please enter the initial coordinates of the end effector in the format (x, y, z) where x,z are the coordinates in millimeters and y is the rotation angle of base in degrees:\n    ", BLUE, RESET);
+    scanf("%d, %d, %d", &initcoords[0], &initcoords[1], &initcoords[2]);
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    You have entered the following initial coordinates:\n    %sX Coordinate:%s %d mm\n    %sBase Rotation Angle:%s %d degrees\n    %sZ Coordinate:%s %d mm\n    %s1)%s Confirm and continue\n    %s2)%s Re-enter initial coordinates\n    ", BLUE, RESET, RED, RESET, initcoords[0], RED, RESET, initcoords[1], RED, RESET, initcoords[2], RED, RESET, RED, RESET);
+    if (scanf("%d", &choice) == 1 && choice == 1) {
+        getTargetCoords();
+    }
+    else if (choice == 2) {
+        getInitialCoords();
+    }
+    else {
+        clearScreen();
+        printf("navn\n");
+    }
 }
 
 void defaultSecLenMenu(){
@@ -32,7 +70,7 @@ void customSecLenMenu() {
     scanf("%d", &tempLengths[1]);
 
     clearScreen();
-    printf("%sCUSTOM SECTION LENGTH MENU%s\n    You have entered the following section lengths:\n    %sLower Section Length:%s %d mm\n    %sUpper Section Length:%s %d mm\n    %s1)%s Confirm and continue\n    %s2)%s Re-enter section lengths\n    ", BLUE, RESET, RED, RESET, tempLengths[0], RED, RESET, tempLengths[1], RED, RESET);
+    printf("%sCUSTOM SECTION LENGTH MENU%s\n    You have entered the following section lengths:\n    %sLower Section Length:%s %d mm\n    %sUpper Section Length:%s %d mm\n    %s1)%s Confirm and continue\n    %s2)%s Re-enter section lengths\n    ", BLUE, RESET, RED, RESET, tempLengths[0], RED, RESET, tempLengths[1], RED, RESET, RED, RESET);
     if (scanf("%d", &choice) == 1 && choice == 1) {
         lengths[0] = tempLengths[0];
         lengths[1] = tempLengths[1];
@@ -44,6 +82,7 @@ void customSecLenMenu() {
         clearScreen();
         printf("navn\n");
     }
+    getCoords();
 }
 
 
